9390af1f45e5c83625a7432fb95d30c7a05a2a1c
Giovanny Oliveira <giovannylucax@gmail.com>
2026-03-03 16:59:29 -0300

    feat: :sparkles: add csrf token validation

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -1,8 +1,11 @@
 package api
 
 import (
+	"os"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/gorilla/csrf"
 )
 
 func (api *Api) BindRoutes() {
@@ -12,8 +15,16 @@ func (api *Api) BindRoutes() {
 	// manage sessions
 	api.Router.Use(api.Sessions.LoadAndSave)
 
+	csrfMiddleware := csrf.Protect(
+		[]byte(os.Getenv("GOBID_CSRF_KEY")),
+		csrf.Secure(os.Getenv("ENV") != "local"),
+		csrf.TrustedOrigins([]string{""}),
+	)
+	api.Router.Use(csrfMiddleware)
+
 	api.Router.Route("/api", func(r chi.Router) {
 		r.Route("/v1", func(r chi.Router) {
+			r.Get("/csrf-token", api.HandleGetCSRFToken)
 			r.Route("/users", func(r chi.Router) {
 				r.Post("/signup", api.handleSignupUser)
 				r.Post("/login", api.handleLoginUser)
